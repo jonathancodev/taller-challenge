9a55d90dfe03d1b17db26a7719caf8be76dfa629
Your Name <you@example.com>
2023-03-14 10:26:01 -0300

    Including challenge file

---
+++ challenge.java
@@ -0,0 +1,27 @@
+package com.averydennison.dcl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class challenge {
+    public static void main(String[] args) {
+        String text = "GEEKSFORGEEKS";
+        int length = text.length();
+        int maxLen = 0;
+
+        Set<Character> set = new HashSet<>();
+
+        for (int i=0; i<length; i++) {
+            char c = text.charAt(i);
+            if (!set.contains(c)){
+                set.add(c);
+            } else {
+                maxLen = Math.max(maxLen, set.size());
+                set.clear();
+            }
+        }
+
+        System.out.println(maxLen);
+
+    }
+}
